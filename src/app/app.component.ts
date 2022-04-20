bbda73806bee3019524ff2cf22cf3f83082c3eae
kathe <Kathe07338@gmail.com>
2022-04-20 22:27:48 +0400

    Crear perfil con los datos basicos, Nombre,carrera, direccion, tel.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'casoIntegrador';
+
+  name: string;
+  email: string;
+  contacto: string;
+  direccion: string;
+  carrera: string
+
+    constructor() {
+
+      this.name = "CRISTIAN GUTERREZ"
+      this.email= "cris123@GMAIL.COM"
+      this.contacto= "+971 5845141123"
+      this.direccion = "Dubai, Emiratos Árabes Unidos"
+      this.carrera = "Senior Frontend Engineer at Banque Saudi Fransi | Life-Career coach"
+
+    }
+
 }
